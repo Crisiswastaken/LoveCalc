fd9b92199e7ec0e30f76132f1d8a256c0ff94831
Crisiswastaken <85402704+Crisiswastaken@users.noreply.github.com>
2024-08-29 23:04:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,9 @@ document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
     userName = document.querySelector("#usersName").value;
     partnerName = document.querySelector("#partnersName").value;
-    if ((userName.toLowerCase() === "vince" && partnerName.toLowerCase() === "prerana") || (userName.toLowerCase() === "prerana" && partnerName.toLowerCase() === "vince")) {
-        document.querySelector("h1").textContent = "Vince and Prerana are 100% compatible!";
-        document.querySelector(".js-text").textContent = "Enter Different Names to try again!";
-        document.querySelector("#usersName").value = "";
-        document.querySelector("#partnersName").value = "";
-    } else {
-        const compatibility = checkCompatibility();
-        document.querySelector("h1").textContent = `${userName} and ${partnerName} are ${compatibility} compatible!`;
-        document.querySelector(".js-text").textContent = "Enter Different Names to try again!";
-        document.querySelector("#usersName").value = "";
-        document.querySelector("#partnersName").value = "";
-    }
+    const compatibility = checkCompatibility();
+    document.querySelector("h1").textContent = `${userName} and ${partnerName} are ${compatibility} compatible!`;
+    document.querySelector(".js-text").textContent = "Enter Different Names to try again!";
+    document.querySelector("#usersName").value = "";
+    document.querySelector("#partnersName").value = "";
 });
